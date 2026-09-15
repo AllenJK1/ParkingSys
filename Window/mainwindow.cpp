1aa14418ef3128b8a02317b08c047ad5868d75d3
AllenK-1 <alienware105@proton.me>
2026-09-15 18:18:23 +0000

    Added user input details form

--- Window/mainwindow.cpp
+++ Window/mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
-
+#include "./ui_CustomerInfo.h"
+#include <QDialog>
+#include <QDebug>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -13,8 +15,26 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
 void MainWindow::on_pushButton_clicked()
 {
-    qDebug() << "Yet to implement primitives for this button";
-}
+    QDialog dialog(this);
+    Ui::Dialog customerUi;
+    customerUi.setupUi(&dialog);
 
+    if (dialog.exec() == QDialog::Accepted) {
+        qDebug() << "OK clicked!";
+        QString FirstName = customerUi.textEdit->toPlainText();
+        QString SecondName = customerUi.textEdit_2->toPlainText();
+        QString VehicleReg = customerUi.textEdit_3->toPlainText();
+        QString IdNumber = customerUi.textEdit_4->toPlainText();
+        qDebug() << "First Name: "  << FirstName;
+        qDebug() << "Second Name: " << SecondName;
+        qDebug() << "Vehicle Reg: " << VehicleReg;
+        qDebug() << "ID Number: "   << IdNumber;
+
+    }
+    else {
+        qDebug() << "User Clicked Cancel";
+    }
+}
